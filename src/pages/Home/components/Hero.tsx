d8dacabde6fedca990eac5bd42a61b1979564004
Baptiste Verdier <hike@MacBook-Air-de-Baptiste.local>
2022-10-25 03:04:21 +0200

    oui

--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -41,7 +41,7 @@ export const Hero = () => {
       <Stack
         h={"full"}
         w={"full"}
-        bgImage={"../../../../../public/assets/wall.png"}
+        backgroundImage={"../../../../../public/assets/wall.png"}
         backgroundSize={"cover"}
         backgroundRepeat={"no-repeat"}
         rounded={"3xl"}
